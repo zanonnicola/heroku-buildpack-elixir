2c10796c8ff868cf10ee6493d6bddcad8fa3e9b6
zanonnicola <nicola.zanon@springernature.com>
2019-07-12 14:08:27 +0200

    Hack

--- lib/elixir_funcs.sh
+++ lib/elixir_funcs.sh
@@ -7,7 +7,7 @@ function download_elixir() {
     elixir_changed=true
     local otp_version=$(otp_version ${erlang_version})
 
-    local download_url="https://repo.hex.pm/builds/elixir/${elixir_version}-otp-${otp_version}.zip"
+    local download_url="https://github.com/elixir-lang/elixir/releases/download/v1.9.0/Precompiled.zip"
 
     output_section "Fetching Elixir ${elixir_version} for OTP ${otp_version} from ${download_url}"
 
